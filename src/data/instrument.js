2bd5e285936faf727d0bd74edc9788d276af5264
Sam Jespersen <heyheysamj@gmail.com>
2019-08-05 15:32:48 -0700

    added instrument array with trumpet object

---
+++ src/data/instrument.js
@@ -0,0 +1,18 @@
+export const instruments = [
+    {
+        name: 'Trumpet',
+        attackLevel: 1,
+        releaseLevel: 0,
+        attackTime: 0.01,
+        decayTime: 0.2,
+        susPercent: 0.75,
+        releaseTime: 2,
+        filterAttackTime: 0.01,
+        filterDecayTime: 0.2,
+        filterSusPercent: .75,
+        filterReleaseTime: 2,
+        filterFrequency: 2000,
+        filterType: 'low pass',
+        oscType: 'sawtooth'
+    }
+];
