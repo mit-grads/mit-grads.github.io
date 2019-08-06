49197dbf38e2365591c69a068311cfd3dc21ceed
Joe Klause <joeklause@gmail.com>
2019-08-06 11:43:36 -0700

    play interval is wired up

---
+++ src/quiz-page/quiz-page.js
@@ -0,0 +1,21 @@
+import { playInterval } from '../playInterval.js';
+import { findById } from '../utils.js';
+import { instruments } from '../data/instrument.js';
+import { IntervalClass } from '../data/interval-class.js';
+
+const playIntervalButton = document.getElementById('play-interval-button');
+
+const interval = new IntervalClass();
+const distance = Math.floor(Math.random() * 8);
+interval.setSecondNote(distance);
+
+const firstNote = interval.getFirstNote();
+const secondNote = interval.getSecondNote();
+
+const instrument = findById(instruments, 'trumpet');
+const duration = 1.5;
+const intervalType = 'melodic';
+
+playIntervalButton.addEventListener('click', () => {
+    playInterval(firstNote, secondNote, instrument, intervalType, duration);
+});
