0dceddf7f343b8e7a0bff9661cf5c50e82eef55f
Dave Trost <davetrost@gmail.com>
2019-08-06 09:57:53 -0700

    refactored intervals class, passing tests

--- src/data/interval-class.js
+++ src/data/interval-class.js
@@ -1,32 +1,36 @@
 import { notes, noteArray } from './notes.js';
 
 export class IntervalClass {
-    constructor(note = 'A1', diatonic = true) {
-        this.lowIndex = noteArray.indexOf(note);
-        this.highIndex = -1;
+    constructor(note = 'A1', scale = 'diatonic') {
+        this.startIndex = noteArray.indexOf(note);
         if(notes[note]) {
-            this.lowNote = note;
+            this.firstNote = note;
         }
         else {
-            this.lowNote = '';
+            this.firstNote = '';
         }
-        this.highNote = '';
-        if(diatonic) {
-            this.halfSteps = [0, 2, 4, 5, 7, 9, 11, 12];
+        this.secondNote = '';
+        if(scale === 'diatonic') {
+            this.scaleDegrees = [0, 2, 4, 5, 7, 9, 11, 12];
         }
         else { 
-            this.halfSteps = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+            this.scaleDegrees = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
         }
     }
 
-    getLowNote() {
-        return noteArray[this.lowIndex];
+    getFirstNote() {
+        return noteArray[this.startIndex];
     }
-    getHighNote() {
-        return this.highNote;
+    getSecondNote() {
+        return this.secondNote;
     }
-    setInterval(distance) {
-        this.highNote = noteArray[this.lowIndex + this.halfSteps[distance]];
+    setSecondNote(distance, direction = 'ascending') {
+        if(direction === 'ascending') {
+            this.secondNote = noteArray[this.startIndex + this.scaleDegrees[distance]];
+        }
+        else {
+            this.secondNote = noteArray[this.startIndex - this.scaleDegrees[distance]];
+        }
     }
     randomizeInterval() {
         this.upperNote = notes['C1'];
--- test/interval-class-tests.js
+++ test/interval-class-tests.js
@@ -1,53 +1,50 @@
 import { IntervalClass } from '../src/data/interval-class.js';
-import { diatonicIntervalReference } from '../src/data/notes.js';
+// import { diatonicIntervalReference } from '../src/data/notes.js';
 
 const test = QUnit.test;
 QUnit.module('Interval Class Tests');
 
 test('test the constructor and setting the start note', assert => {
-    const interval = new IntervalClass('A1', true);
-    assert.equal(interval.getLowNote(), 'A1');
+    const interval = new IntervalClass('A1', 'diatonic');
+    assert.equal(interval.getFirstNote(), 'A1');
 
-    const interval2 = new IntervalClass('G2', true);
-    assert.equal(interval2.getLowNote(), 'G2');
+    const interval2 = new IntervalClass('G2', 'diatonic');
+    assert.equal(interval2.getFirstNote(), 'G2');
 });
 
 test('test creation of intervals with diatonic tones', assert => {
-    const interval = new IntervalClass('A1', true);
+    const interval = new IntervalClass('A1', 'diatonic');
 
-    interval.setInterval(diatonicIntervalReference['whole tone']);
-    assert.equal(interval.highNote, 'B1');
-
-    interval.setInterval(4);
-    assert.equal(interval.highNote, 'E1');
+    interval.setSecondNote(4);
+    assert.equal(interval.secondNote, 'E1');
 });    
 
-test('test creation of intervals with chromatic tones', assert => {
-    const interval = new IntervalClass('A1', false);
+// test('test creation of intervals with chromatic tones', assert => {
+//     const interval = new IntervalClass('A1', false);
 
-    interval.setInterval(12);
-    assert.equal(interval.highNote, 'A2');
+//     interval.setSecondNote(12);
+//     assert.equal(interval.secondNote, 'A2');
 
-    interval.setInterval(7);
-    assert.equal(interval.highNote, 'E1');
-});    
+//     interval.setSecondNote(7);
+//     assert.equal(interval.secondNote, 'E1');
+// });    
 
 test('test creation of intervals with diatonic tones - and with different starting notes', assert => {
-    const interval = new IntervalClass('C1', true);
-    interval.setInterval(7);
-    assert.equal(interval.highNote, 'C2');
+    const interval = new IntervalClass('C1', 'diatonic');
+    interval.setSecondNote(7);
+    assert.equal(interval.secondNote, 'C2');
     
-    const interval2 = new IntervalClass('G1', true);
-    interval2.setInterval(4);
-    assert.equal(interval.highNote, 'C2');
+    const interval2 = new IntervalClass('G1', 'diatonic');
+    interval2.setSecondNote(4);
+    assert.equal(interval.secondNote, 'C2');
 });    
 
-test('test creation of intervals with chromatic tones - and with different starting notes', assert => {
-    const interval = new IntervalClass('C1', false);
-    interval.setInterval(12);
-    assert.equal(interval.highNote, 'C2');
+// test('test creation of intervals with chromatic tones - and with different starting notes', assert => {
+//     const interval = new IntervalClass('C1', false);
+//     interval.setSecondNote(12);
+//     assert.equal(interval.highNote, 'C2');
     
-    const interval2 = new IntervalClass('G1', false);
-    interval2.setInterval(10);
-    assert.equal(interval.highNote, 'C2');
-});    
+//     const interval2 = new IntervalClass('G1', false);
+//     interval2.setSecondNote(10);
+//     assert.equal(interval.highNote, 'C2');
+// });    
