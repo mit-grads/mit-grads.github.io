e5eae733a1e3ae3deeb768b77eeaf255e9c7eb59
Sam Jespersen <heyheysamj@gmail.com>
2019-08-05 17:06:23 -0700

    play note functionality

--- src/data/instrument.js
+++ src/data/instrument.js
@@ -1,18 +1,19 @@
 export const instruments = [
     {
+        id: 'trumpet',
         name: 'Trumpet',
+        filterType: 'low pass',
+        oscType: 'sawtooth',
         attackLevel: 1,
         releaseLevel: 0,
+        filterAttackLevel: 1,
+        filterReleaseLevel: 0,
         attackTime: 0.01,
         decayTime: 0.2,
         susPercent: 0.75,
-        releaseTime: 2,
         filterAttackTime: 0.01,
         filterDecayTime: 0.2,
         filterSusPercent: .75,
-        filterReleaseTime: 2,
         filterFrequency: 2000,
-        filterType: 'low pass',
-        oscType: 'sawtooth'
     }
 ];
---
+++ src/playNote.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import { findById } from '../src/utils.js';
+import { noteArray } from '../src/data/notes.js';
+import { instruments } from '../src/data/instrument.js';
+
+const button = document.getElementById('bweh');
+
+const instrument = findById(instruments, 'trumpet');
+
+
+button.addEventListener('click', () => {
+    playNote(instrument, 'A1', 2);
+});
+
+
+function playNote(instrument, note, duration) {
+
+    let env = new p5.Envelope();
+    let filterEnv = new p5.Envelope();
+    let osc = new p5.Oscillator();
+    let filter;
+
+    switch(instrument.filterType) {
+        case 'low pass':
+            filter = new p5.LowPass();
+            break;
+        case 'high pass':
+            filter = new p5.HighPass();
+            break;
+        case 'band pass':
+            filter = new p5.BandPass();
+            break;
+    }
+    let attackLevel = instrument.attackLevel;
+    let releaseLevel = instrument.releaseLevel;
+    let filterAttackLevel = instrument.filterAttackLevel;
+    let filterReleaseLevel = instrument.filterReleaseLevel;
+
+    let oA = instrument.attackTime;
+    let oD = instrument.decayTime;
+    let oS = instrument.susPercent;
+    let oR = duration;
+
+    let fA = instrument.filterAttackTime;
+    let fD = instrument.filterDecayTime;
+    let fS = instrument.filterSusPercent;
+    let fR = duration;
+
+    filter.freq(instrument.filterFrequency);
+
+    osc.stop();
+    osc.start();
+
+    env.setADSR(oA, oD, oS, oR);
+    env.setRange(attackLevel, releaseLevel);
+
+    filterEnv.setADSR(fA, fD, fS, fR);
+    filterEnv.setRange(filterAttackLevel, filterReleaseLevel);
+
+    osc.disconnect();
+    osc.connect(filter);
+
+    osc.setType(instrument.oscType);
+    osc.amp(.5);
+
+    const frequency = noteArray[note];
+    osc.freq(frequency);
+
+    filterEnv.play(filter);
+    env.play(osc);
+}
