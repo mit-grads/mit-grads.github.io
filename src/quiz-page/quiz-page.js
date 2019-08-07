384456c75ccb8505e3d106e4ff5a96c73e8b48ca
Sam Jespersen <heyheysamj@gmail.com>
2019-08-07 11:55:38 -0700

    added results object array and fixed button problems

--- src/quiz-page/quiz-page.js
+++ src/quiz-page/quiz-page.js
@@ -1,5 +1,5 @@
 import { playInterval } from '../playInterval.js';
-import { findById } from '../utils.js';
+import { findById, shuffle } from '../utils.js';
 import { instruments } from '../data/instrument.js';
 import { IntervalClass } from '../data/interval-class.js';
 import { renderAnswerOption } from '../render-answer-options.js';
@@ -8,46 +8,65 @@ import { GenerateInterval } from '../quiz-page/generate-interval.js';
 
 const playIntervalButton = document.getElementById('play-interval-button');
 const choiceSection = document.getElementById('choice-section');
+const nextButton = document.getElementById('next-button');
 
 const interval = new IntervalClass();
-const distance = Math.floor(Math.random() * 8);
-interval.setSecondNote(distance);
 
-const firstNote = interval.getFirstNote();
-const secondNote = interval.getSecondNote();
+let roundCounter = 0;
+let correctAnswer;
+let playCallback;
 
-const instrument = findById(instruments, 'trumpet');
-const duration = 1.5;
-const intervalType = 'melodic';
-
-playIntervalButton.addEventListener('click', () => {
-    playInterval(firstNote, secondNote, instrument, intervalType, duration);
-});
+quizRound();
 
 
-let answerOptionsArray = [];
 
-const option = new GenerateInterval(interval.scale);
-const correctAnswer = option.scale[distance];
-option.removeInterval(correctAnswer);
+function quizRound() {
+    const distance = Math.floor(Math.random() * 8);
+    interval.setSecondNote(distance);
 
-const answer1 = option.getRandomInterval();
-const answer2 = option.getRandomInterval();
+    const firstNote = interval.getFirstNote();
+    const secondNote = interval.getSecondNote();
 
-answerOptionsArray.push(correctAnswer);
-answerOptionsArray.push(answer1);
-answerOptionsArray.push(answer2);
 
-answerOptionsArray = shuffle(answerOptionsArray);
+    const instrument = findById(instruments, 'trumpet');
+    const duration = 1.5;
+    const intervalType = 'melodic';
 
-for(let i = 0; i < answerOptionsArray.length; i++) {
-    if(i === 0) {
-        const dom = renderAnswerOption(answerOptionsArray[i], true);
-        choiceSection.appendChild(dom);
-    } else {
-        const dom = renderAnswerOption(answerOptionsArray[i], false);
-        choiceSection.appendChild(dom);
+    playIntervalButton.removeEventListener('click', playCallback);
+
+    playCallback = () => {
+        playInterval(firstNote, secondNote, instrument, intervalType, duration);
+
+    };
+
+
+    playIntervalButton.addEventListener('click', playCallback);
+
+    let answerOptionsArray = [];
+
+    const option = new GenerateInterval(interval.scale);
+    correctAnswer = option.scale[distance];
+    option.removeInterval(correctAnswer);
+
+    const answer1 = option.getRandomInterval();
+    const answer2 = option.getRandomInterval();
+
+    answerOptionsArray.push(correctAnswer);
+    answerOptionsArray.push(answer1);
+    answerOptionsArray.push(answer2);
+
+    answerOptionsArray = shuffle(answerOptionsArray);
+
+    for(let i = 0; i < answerOptionsArray.length; i++) {
+        if(i === 0) {
+            const dom = renderAnswerOption(answerOptionsArray[i], true);
+            choiceSection.appendChild(dom);
+        } else {
+            const dom = renderAnswerOption(answerOptionsArray[i], false);
+            choiceSection.appendChild(dom);
+        }
     }
+
 }
 
 const answerButtons = document.getElementsByClassName('answer-button');
@@ -62,12 +81,74 @@ const answerButtons = document.getElementsByClassName('answer-button');
 
 
 
-// Fisher-Yates Shuffle. Source: https://javascript.info/task/shuffle
 
-function shuffle(arr) { 
-    for(let i = arr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
+
+let resultsArray = [];
+
+nextButton.addEventListener('click', (playCallback) => {
+
+    let selectedButton;
+    const buttons = [...answerButtons];
+    for(let i = 0; i < buttons.length; i++) {
+        if(buttons[i].className === 'answer-button selected') {
+            selectedButton = buttons[i].id;
+        }
     }
-    return arr;
-}
+
+
+
+    for(let i = 0; i <= resultsArray.length; i++) {
+
+        if(resultsArray.length > 0) {
+
+            if(resultsArray[i].interval === selectedButton) {
+                resultsArray[i].attempts++;
+
+                if(selectedButton === correctAnswer) {
+                    resultsArray[i].correct++;
+                }
+                break;
+            } else {
+                const answerObj = {
+                    interval: selectedButton,
+                    correct: 0,
+                    attempts: 1
+                };
+
+                if(selectedButton === correctAnswer) {
+                    answerObj.correct++;
+                }
+                resultsArray.push(answerObj);
+                break;
+            }
+        } else {
+            const answerObj = {
+                interval: selectedButton,
+                correct: 0,
+                attempts: 1
+            };
+
+            if(selectedButton === correctAnswer) {
+                answerObj.correct++;
+            }
+            resultsArray.push(answerObj);
+            break;
+
+
+        }
+
+    }
+
+    while(choiceSection.firstChild) {
+        choiceSection.removeChild(choiceSection.firstChild);
+    }
+
+
+
+    roundCounter++;
+    console.log(roundCounter, resultsArray);
+    quizRound();
+
+});
+
+
