a6690908430820747a2eac355d9982c4efd3ae50
Phil Fitch <mostreluctantly@gmail.com>
2019-08-07 10:51:53 -0700

    results-page passing tests

--- test/render-results-page.test.js
+++ test/render-results-page.test.js
@@ -1,32 +1,62 @@
-import { renderTable } from '../src/results-page/results-page.js';
-import { storage } from '../src/data/storage.js';
+import { renderTHead, renderTBody, renderTFoot } from '../src/results-page/results-page.js';
 
 const test = QUnit.test;
 
 QUnit.module('render results page');
 
-test('render results page html', assert => {
-    // arrange
-    const expected = '<thead><th>Interval</th><th>Correct</th><th>Attempts</th></thead><tbody><tr><td>Major 3rd</td><td>2</td><td>3</td></tr><tr><td>Perfect 4th</td><td>1</td><td>3</td></tr><tr><td>Major 7th</td><td>2</td><td>4</td></tr></tbody><tfoot><th>Total</th><th>5</th><th>10</th></tfoot>';
+test('render THead html', assert => {
+    const expected = '<thead><th>Interval</th><th>Correct</th><th>Attempts</th></thead>';
+    
+    const dom = renderTHead();
+    const html = dom.outerHTML;
+    
+    assert.equal(html, expected);
+});
+
+
+test('render TBody html', assert => {
+    const expected = '<tbody><tr><td>Major 3rd</td><td>2</td><td>3</td></tr><tr><td>Perfect 4th</td><td>1</td><td>3</td></tr><tr><td>Major 7th</td><td>2</td><td>4</td></tr></tbody>';
     const results = [{
         interval: 'Major 3rd',
-        correctCount: 2,
+        correct: 2,
         attempts: 3
     }, {
         interval: 'Perfect 4th',
-        correctCount: 1,
+        correct: 1,
         attempts: 3
     }, {
         interval: 'Major 7th',
-        correctCount: 2,
+        correct: 2,
         attempts: 4
     }];
-    storage.saveQuizResults(results);
 
-    // act
-    const dom = renderTable();
+    const dom = renderTBody(results);
     const html = dom.outerHTML;
-    
-    // assert
+
     assert.equal(html, expected);
 });
+
+
+test('render TFoot html', assert => {
+    
+    const expected = '<tfoot><th>Total</th><th>5</th><th>10</th></tfoot>';
+    const results = [{
+        interval: 'Major 3rd',
+        correct: 2,
+        attempts: 3
+    }, {
+        interval: 'Perfect 4th',
+        correct: 1,
+        attempts: 3
+    }, {
+        interval: 'Major 7th',
+        correct: 2,
+        attempts: 4
+    }];
+
+    const dom = renderTFoot(results);
+    const html = dom.outerHTML;
+
+    assert.equal(html, expected);
+});
+
